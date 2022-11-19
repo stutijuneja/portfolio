1e50d278b24adcd1828d0b630d3c50fbc6e903a2
stutijuneja <98301261+stutijuneja@users.noreply.github.com>
2022-11-19 20:40:55 +0530

    new code with optimization

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-$(document).ready(function () {
-  $("#menu").click(function () {
-    $(this).toggleClass("fa-times");
-    $("header").toggleClass("toggle");
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
   });
 
-  $(window).on("scroll load", function () {
-    $("#menu").removeClass("fa-times");
-    $("header").removeClass("toggle");
+  $(window).on('scroll load',function(){
 
-    if ($(window).scrollTop() > 0) {
-      $(".top").show();
-    } else {
-      $(".top").hide();
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
     }
+
   });
 
-  // smooth scrolling
+  // smooth scrolling 
 
-  $('a[href*="#"]').on("click", function (e) {
+  $('a[href*="#"]').on('click',function(e){
+
     e.preventDefault();
 
-    $("html, body").animate(
-      {
-        scrollTop: $($(this).attr("href")).offset().top,
-      },
-      500,
-      "linear"
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
     );
+
   });
+
 });
